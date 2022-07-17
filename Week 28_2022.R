5046613376424448435391a0ee54d74677c1cb8c
Stephan Teodosescu <32054537+steodose@users.noreply.github.com>
2022-07-16 17:10:39 -0700

    Update Turkey name in code

--- Week 28_2022.R
+++ Week 28_2022.R
@@ -12,6 +12,7 @@ library(ggimage) #for working with logos
 library(janitor)
 library(lubridate)
 library(zoo)
+library(scales)
 
 
 # Repo link: https://github.com/rfordatascience/tidytuesday/tree/master/data/2022/2022-07-12
@@ -93,7 +94,7 @@ country_colors <- c(
     'Spain' = "#F1BF00",
     'France' = "#002654",
     'Italy' = "#008C45", 
-    'Türkiye' = "#C8102E",
+    'Turkey' = "#C8102E",
     'Norway' = "#BA0C2F",
     'Netherlands' = "#F36C21",
     'Portugal' = "#046A38",
@@ -105,6 +106,13 @@ country_colors <- c(
 description_color <- 'grey40'
 
 ## Data preparation
+
+# change Turkey's name as the formatting doesn't play nice with code
+flights <- flights %>%
+    mutate(state_name = case_when(
+        state_name == 'Turkey' ~ 'Turkey',
+                           TRUE ~ state_name))
+
 
 country_flights_by_year <- flights %>% 
     select(year, state = state_name, flights = flt_tot_1) %>% 
@@ -127,9 +135,9 @@ todays_top <- country_rank_by_year %>%
 
 
 
-## Make bump plot
+## 1. ------------- Make bump plot ------------------
 
-country_rank_by_year %>% 
+flights_bump_chart <- country_rank_by_year %>% 
     filter(state %in% todays_top) %>% 
     ggplot(aes(year, rank, col = state)) + 
     geom_point(size = 2) +
@@ -191,29 +199,16 @@ country_rank_by_year %>%
     labs(x = "",
          y = "",
         title = 'Top 12 European Countries for Flights',
-        subtitle = 'Ranked by number of incoming and outgoing flights from 2016-2022. 2022 data thru May.',
-        caption = "TidyTuesday 2022 - Week 28 | Source: Eurocontrol | Plot: @steodosescu"
+        subtitle = 'Ranked by number of incoming and outgoing flights from 2016-2022. 2022 data thru May.'
     )
 
+flights_bump_chart
 
 ggsave("Flight Bumps Chart.png")
 
 
-# Add logo to plot
-flight_bumps_chart_with_logo <- add_logo(
-    plot_path = "/Users/Stephan/Desktop/R Projects/Tidy-Tuesday/Flight Bumps Chart.png", # url or local file for the plot
-    logo_path = "/Users/Stephan/Desktop/R Projects/Tidy-Tuesday/europe.png", # url or local file for the logo
-    logo_position = "top left", # choose a corner
-    # 'top left', 'top right', 'bottom left' or 'bottom right'
-    logo_scale = 20
-)
 
-# save the image and write to working directory
-magick::image_write(flight_bumps_chart_with_logo, "Flight Bumps Chart with Logo.png")
-
-
-
-## Build small multiples chart
+## 2. -------------- Build small multiples chart ----------------
 
 # prepare data
 flights_grouped <- flights %>% 
@@ -231,51 +226,120 @@ flights_grouped$stateDuplicate <- flights_grouped$state
 # Make flag colors and logo df
 
 flag_colors <- tibble(
-    state = c('United Kingdom','Germany', 'Spain', 'France', 'Italy', 'Türkiye', 
-              'Netherlands', 'Norway', 'Switzerand', 'Greece', 'Portugal', 'Poland'),
+    state = c('United Kingdom','Germany', 'Spain', 'France', 'Italy', 'Turkey', 
+              'Netherlands', 'Norway', 'Switzerland', 'Greece', 'Portugal', 'Poland'),
     state_abbr = c('UK', 'DE', 'ES', 'FR', 'IT', 'TK', 'NL', 'NO', 'CH', 'GR', 'PL', 'PO'),
-    flag_color = c("#012169","#DD0000")
+    color = c("#012169","#DD0000","#F1BF00","#002654","#008C45","#C8102E","#F36C21",
+                   "#BA0C2F","#DA291C","#001489","#046A38","#DC143C")
     )
 
-'United Kingdom' = "#012169",
-'Germany' = "#DD0000",
-'Spain' = "#F1BF00",
-'France' = "#002654",
-'Italy' = "#008C45", 
-'Türkiye' = "#C8102E",
-'Norway' = "#BA0C2F",
-'Netherlands' = "#F36C21",
-'Portugal' = "#046A38",
-'Switzerland' = "#DA291C",
-'Poland' = "#DC143C",
-'Greece' = "#001489"
+flag_colors <- flag_colors %>% 
+    mutate('flag_logo' = paste0('https://raw.githubusercontent.com/steodose/Tidy-Tuesday/master/flags/', state, '.png'))
 
 # filter for 12 countries and join flag icons
+flights_grouped2 <- flights_grouped %>% 
+    filter(state %in% todays_top)
 
-flights_grouped2
+flights_grouped2 <- flights_grouped2 %>% 
+    left_join(flag_colors, by = 'state')
+    
 
 # make chart
 p1 <- flights_grouped2 %>% 
     ggplot(aes(x = date, y = flights)) + 
     #geom_smooth(data = mutate(team_games2, home = NULL), aes(group = teamDuplicate), method = "lm", formula = y ~ splines::bs(x, 5), se = FALSE, colour = 'grey80', size = .25, alpha = .5) +
     #geom_smooth(aes(group = team, color = team_color1), method = "lm",  formula = y ~ splines::bs(x, 5), se = FALSE, size = .5, alpha = 1, show.legend = FALSE) +
-    geom_line(data = mutate(flights_grouped2, state_abbr = NULL), aes(group = stateDuplicate), colour = 'grey80', size = .25, alpha = .5) +
-    geom_line(aes(group = state, color = team_color1), size = .5, alpha = 1, show.legend = FALSE) +
+    geom_line(data = mutate(flights_grouped2, state = NULL), aes(group = stateDuplicate), colour = 'grey80', size = .25, alpha = .5) +
+    geom_line(aes(group = state, color = flights_grouped2$color), size = .5, alpha = 1, show.legend = FALSE) +
     scale_color_identity() +
     facet_wrap(~fct_reorder(state, -flights)) +
     theme_custom() + 
-    geom_hline(yintercept = 0, linetype = "dashed", color = "#a39d9d") +
     theme(plot.title.position = 'plot', 
-          plot.title = element_text(face = 'bold'), 
-          plot.margin = margin(10, 10, 15, 10), 
+          plot.title = element_text(
+              face = 'bold',
+              size = 20,
+              hjust = 0.5
+          ),
+          plot.subtitle = element_text(
+              margin = margin(t = 3, b = 2, unit = 'mm'),
+              hjust = 0.5
+          ),
+          plot.margin = margin(10, 10, 15, 10),
+          axis.text.x=element_blank(),
           panel.spacing = unit(0.5, 'lines')) +
     scale_y_continuous(
-        labels = signs::signs_format(add_plusses = TRUE), #trick to get + in front of positive numbers
-        breaks = seq(-100, 100, 50)) +
-    labs(x = "Game No.", 
-         y = "Goal Differential", 
-         title = "2021-22 NHL Goal Differential",
-         subtitle = glue("Teams sorted by total goal differential thru {latest_game} games."),
-         caption = "Data: hockeyR\nGraphic: @steodosescu") +
+        labels = scales::comma) +
+    labs(x = "", 
+         y = "Flights per Month", 
+         title = "Total Flights per Month",
+         subtitle = "Ranked by total number of incoming and outgoing flights from Jan 2016 thru May 2022.",
+         caption = "TidyTuesday 2022 - Week 28 | Source: Eurocontrol | Plot: @steodosescu") +
     theme(plot.title = element_markdown()) +
     theme(plot.subtitle = element_markdown())
+
+p1
+
+ggsave("Flights Facet Chart.png")
+
+# add logos to each facet 
+
+## Reference: https://github.com/tonyelhabr/sports_viz/blob/master/42-202122_new_players/01-main.R
+p_bld <- ggplot_gtable(ggplot_build(p1))
+grob_strip_index <- which(sapply(p_bld$grob, function(x) x$name)=='strip')
+facet_id <- sapply(grob_strip_index, function(grb) {
+    p_bld$grobs[[grb]]$grobs[[1]]$children[[2]]$children[[1]]$label
+})
+# p_bld$layout$z[grob_strip_index] <- 0 ## not sure what the point of this is...
+
+for (i in 1:length(facet_id)) {
+    id <- facet_id[i]
+    url <-
+        flag_colors %>% filter(state == !!id) %>% pull(flag_logo)
+    lab <-
+        grid::textGrob(
+            id,
+            x = unit(0, 'npc'),
+            gp = grid::gpar(
+                col = 'black',
+                fontfamily = 'Outfit',
+                fontface = 'bold',
+                fontsize = 8
+            ),
+            hjust = 0
+        )
+    img <-
+        grid::rasterGrob(
+            image = magick::image_read(url),
+            # just = 'right',
+            hjust = 1,
+            x = unit(1, 'npc'),
+            ## 1 and 0.75 is also fine
+            vp = grid::viewport(height = 1, width = 0.75)
+        )
+    tot_tree <- grid::grobTree(lab, img)
+    p_bld$grobs[[grob_strip_index[i]]] <- tot_tree
+}
+
+p1 <- cowplot::ggdraw(p_bld)
+
+ggsave("Flights Facet Chart.png", p1, w = 6, h = 6, dpi = 300)
+
+
+
+## Combine plots with patchwork
+
+flights_bump_chart/p1
+
+ggsave("European Flights Patchwork.png", width = 8, height = 10)
+
+# Add logo to plot
+flight_bumps_chart_with_logo <- add_logo(
+    plot_path = "/Users/Stephan/Desktop/R Projects/Tidy-Tuesday/European Flights Patchwork.png", # url or local file for the plot
+    logo_path = "/Users/Stephan/Desktop/R Projects/Tidy-Tuesday/europe.png", # url or local file for the logo
+    logo_position = "top left", # choose a corner
+    # 'top left', 'top right', 'bottom left' or 'bottom right'
+    logo_scale = 20
+)
+
+# save the image and write to working directory
+magick::image_write(flight_bumps_chart_with_logo, "European Flights Patchwork with Logo.png")
